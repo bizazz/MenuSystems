31557c29f6260c1d8be3adee74234ee954cf0df9
bizazz <jason@bizazz.com>
2014-04-22 12:36:04 -0500

    New brand name

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="author" content="">
     <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
 
-    <title>Bizazz Menu Systems</title>
+    <title>MenuSystems</title>
 
     <!-- Bootstrap core CSS -->
     <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
